efea8631c1b5c8f952ce01631a398d6a9e29a63d
RandyRand0m <146391529+RandyRand0m@users.noreply.github.com>
2023-12-18 12:39:03 +0500

    es6.js

--- src/es6.js
+++ src/es6.js
@@ -7,8 +7,6 @@
 // строку формата Имя Фамилия
 function fioToName(fio) {
     const [lastName, firstName, middleName] = fio.split(' ');
-
-  // Проверяем, есть ли отчество
   if (middleName) {
     return `${firstName} ${lastName}`;
   } else {
@@ -19,23 +17,22 @@ function fioToName(fio) {
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {return Array.from(new Set(array));}
+function filterUnique(array) {
+    return Array.from(new Set(array));
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
 function calculateSalaryDifference(array) {
-    // Проверяем, что массив не пуст
   if (array.length === 0) {
     return false;
   }
 
-  // Находим максимальное и минимальное значение в массиве
   const maxSalary = Math.max(...array);
   const minSalary = Math.min(...array);
 
-  // Вычисляем разницу между максимальной и минимальной зарплатой
   const difference = maxSalary / minSalary;
 
   return difference;
@@ -53,7 +50,6 @@ class Dictionary {
     
       // Добавление слова в словарь
       addWord(key, value) {
-        // Проверка на null и undefined
         if (key != null && value != null) {
           // Проверка, что ключ и значение являются строками
           if (typeof key === 'string' && typeof value === 'string') {
@@ -66,7 +62,6 @@ class Dictionary {
         }
       }
     
-      // Получение значения по ключу
       getWord(key) {
         return this.wordsMap.get(key);
       }
